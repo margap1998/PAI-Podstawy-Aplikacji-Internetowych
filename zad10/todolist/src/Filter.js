8abe628dfc55ee1dbe125ad2f1812794b00060b5
abcd <abcd@CHATAMARIANA>
2020-06-06 14:40:08 +0200

    Update Filter.js

--- zad10/todolist/src/Filter.js
+++ zad10/todolist/src/Filter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 class Filter extends React.Component{
-	toErase;
 	constructor(props){
 		super(props);
 		this.routine = this.routine.bind(this);
@@ -13,7 +12,7 @@ class Filter extends React.Component{
 		return (
 		<div className={"com"}>
 		<div className={"compint"}>
-			<input type="checkbox" onChange= {this.routine}>
+			<input type="checkbox" onChange= {this.routine} checked={this.props.filter}>
 			</input>
 			<label>Hide completed</label>
 		</div></div>)
